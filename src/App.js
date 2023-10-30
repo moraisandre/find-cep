7c1913d2ec73f6e081dd0f64b56e7bc175ccd58a
moraisandre <andrewinvista@gmail.com>
2023-10-30 13:50:16 -0300

    Added API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 import StreetNameSign from './components/StreetNameSign';
 
 function App() {
+
+  const [address, setAddress] = useState({})
+
+  function handleAddress(event) {
+
+    const cep = event.target.value;
+
+    // CEP examples:
+    // 01001000
+    // 03650010
+    // 01302907
+    // 03307005     
+
+    if (cep && cep.length === 8) {
+      fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then(response => response.json())
+        .then(data => {
+          setAddress({
+            cep,
+            logradouro: data.logradouro,
+            bairro: data.bairro,
+            cidade: data.localidade,
+            uf: data.uf,
+          })
+        })
+
+    }
+  }
+
   return (
     <div className="App">
-        <input placeholder="Digite o CEP"></input>
-        <StreetNameSign />
+        <input
+          placeholder="Digite o CEP"
+          maxLength="8"
+          onChange={handleAddress}
+        />
+        <StreetNameSign address={address} />
     </div>
   );
 }
--- src/components/StreetNameSign/index.js
+++ src/components/StreetNameSign/index.js
@@ -1,13 +1,18 @@
 import './StreetNameSign.css'
 
-function StreetNameSign() {
+const StreetNameSign = (props) => {
+
+    const normalizeCep = props.address.cep
+        ? `${props.address.cep.substring(0,5)}-${props.address.cep.substring(5,8)}`
+        : '00000-000'
+
     return (
         <div className="street-name-sign">
-            <div className="logradouro">Logradouro</div>
-            <div className="bairro">bairro</div>
+            <div className="logradouro">{props.address.logradouro}</div>
+            <div className="bairro">{props.address.bairro}</div>
             <div className="sign-details">
-                <div className="bottom-text-detail">localidade - uf</div>
-                <div className="bottom-text-detail">00000-000</div>    
+                <div className="bottom-text-detail">{props.address.cidade} - {props.address.uf}</div>
+                <div className="bottom-text-detail">{normalizeCep}</div>
             </div>
         </div>
     );
